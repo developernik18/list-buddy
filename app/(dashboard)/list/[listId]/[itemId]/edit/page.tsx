126f9a7b2914fdcb674eb0fe52e2a652d928da09
Nikhil Kumar <developernik18@gmail.com>
2023-10-11 14:11:43 +0530

    Fixed type error for list and itesm

--- app/(dashboard)/list/[listId]/[itemId]/edit/page.tsx
+++ app/(dashboard)/list/[listId]/[itemId]/edit/page.tsx
@@ -1,24 +1,30 @@
 import Link from "next/link";
 import { FiArrowLeft } from "react-icons/fi";
-import { getListInfo } from "@/util/server-functions/getListInfo";
+import { addListOrigin, getListInfo } from "@/util/server-functions/getListInfo";
 import ItemEditForm from "./ItemEditForm";
-import { List } from "@/types/list";
 import { getItemDetails } from "@/util/server-functions/getItemDetails";
 import { Item } from "@/types/item";
+import { ListWithoutOrigin } from "@/types/list";
 
 export default async function EditItem({
   params,
 }: {
   params: { listId: number; itemId: number };
 }) {
-  const { data: listDetails, errorMessage: listErrorMessage } =
-    await getListInfo(params.listId);
+  const { 
+    data: list, 
+    errorMessage: listErrorMessage 
+  } = await getListInfo(params.listId);
 
-  const itemDetails: Item = await getItemDetails(
-    listDetails.list_key,
-    params.listId,
-    params.itemId
-  );
+  let itemDetails: Item | null = null;
+
+  if(list) {
+    itemDetails = await getItemDetails(
+      list.list_key,
+      params.listId,
+      params.itemId
+    );
+  }
 
   return (
     <main className="bg-gray-50 h-[90vh]">
@@ -26,24 +32,27 @@ export default async function EditItem({
         <Link href={"/list/" + params.listId} className="w-10">
           <FiArrowLeft />
         </Link>
+        
+        {list && (
+          <section className="w-full lg:w-4/5 xl:w-3/5 mx-auto">
+            <div className="top bg-orange-100 p-3 text-center font-medium text-xl">
+              Edit
+              <span className="text-secondary-default">
+                {' "' + itemDetails?.name + '" '}
+              </span>
+              from
+              <span className="text-secondary-default">
+                {' "' + list?.title + '" '}
+              </span>
+              List
+            </div>
 
-        <section className="w-full lg:w-4/5 xl:w-3/5 mx-auto">
-          <div className="top bg-orange-100 p-3 text-center font-medium text-xl">
-            Edit
-            <span className="text-secondary-default">
-              {' "' + itemDetails?.name + '" '}
-            </span>
-            from
-            <span className="text-secondary-default">
-              {' "' + listDetails?.title + '" '}
-            </span>
-            List
-          </div>
+            {itemDetails && (
+              <ItemEditForm listDetails={addListOrigin(list)} itemDetails={itemDetails} />
+            )}
+          </section>
+        )}
 
-          {itemDetails && (
-            <ItemEditForm listDetails={listDetails} itemDetails={itemDetails} />
-          )}
-        </section>
       </section>
     </main>
   );
