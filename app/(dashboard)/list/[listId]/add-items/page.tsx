a5a3ac3bb6beae2564a463564e387867b12ebbea
Nikhil Kumar <developernik18@gmail.com>
2023-10-11 14:18:55 +0530

    Fixed type error for Item form page

--- app/(dashboard)/list/[listId]/add-items/page.tsx
+++ app/(dashboard)/list/[listId]/add-items/page.tsx
@@ -5,8 +5,9 @@ import { getListInfo } from "@/util/server-functions/getListInfo";
 
 
 export default async function AddToList({ params }: {params: {listId: number}}) {
-  const { data: listInfo, errorMessage: listErrorMessage } =
+  const { data: list, errorMessage: listErrorMessage } =
   await getListInfo(params.listId);
+
 
 
   return (
@@ -16,17 +17,20 @@ export default async function AddToList({ params }: {params: {listId: number}}) 
           <FiArrowLeft />
         </Link>
 
-        <section className="w-full lg:w-4/5 xl:w-3/5 mx-auto">
-          <div className="top bg-orange-100 p-3 text-center font-medium text-xl">
-            Add to 
-            <span className="text-secondary-default">
-                {' "' + listInfo?.title + '" '} 
-            </span>  
-            List
-          </div>
+        {list && (
+          <section className="w-full lg:w-4/5 xl:w-3/5 mx-auto">
+            <div className="top bg-orange-100 p-3 text-center font-medium text-xl">
+              Add to 
+              <span className="text-secondary-default">
+                  {' "' + list?.title + '" '} 
+              </span>  
+              List
+            </div>
 
-          <ItemAdditionForm listId={params.listId} listKey={listInfo.list_key}/>
-        </section>
+            <ItemAdditionForm listId={params.listId} listKey={list.list_key}/>
+          </section>
+        )}
+
 
       </section>
     </main>
